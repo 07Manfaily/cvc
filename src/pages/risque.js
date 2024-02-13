5f3a339336d2fee7041dc90f82d0c7982aa1b342
Manfaily Roland Yao <68975545+07Manfaily@users.noreply.github.com>
2024-02-13 12:03:18 +0000

    Update risque.js

--- src/pages/risque.js
+++ src/pages/risque.js
@@ -1,3 +1,7 @@
+react-dom.development.js:28439  Uncaught Error: Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: undefined. You likely forgot to export your component from the file it's defined in, or you might have mixed up default and named imports.
+
+Check the render method of `Risque`.
+
 import React from 'react';
 import ReactTable from 'react-table';
 import { Link } from 'react-router-dom';

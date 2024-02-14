bd25cf2fb44c58007310dcddf22003c5430e7dc3
Manfaily Roland Yao <68975545+07Manfaily@users.noreply.github.com>
2024-02-14 11:05:02 +0000

    Update Risque.js

--- src/pages/Risque.js
+++ src/pages/Risque.js
@@ -191,4 +191,109 @@ console.log("data legth", data.length);
   );
 }
 
+import React, { useState } from 'react';
+import DataTable from 'react-data-table-component';
+import { useNavigate } from 'react-router-dom';
+import { Card, Stack, Container, Typography, Button } from '@mui/material';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import CircleRoundedIcon from '@mui/icons-material/CircleRounded';
+import Scrollbar from '../components/scrollbar';
+import FilterComponent from '../utils/filter';
+
+export default function Risque() {
+  const navigate = useNavigate();
+
+  const columns = [
+    {
+      name: 'N° client',
+      selector: row => row.Nc,
+    },
+    {
+      name: 'Segment',
+      selector: row => row.seg,
+      Cell: (props) => <span className="number">{props.value}</span>,
+    },
+    {
+      name: 'FCCCR',
+      selector: row => row.FC,
+    },
+    {
+      name: 'Chargé client',
+      selector: row => row.Chgc,
+    },
+    {
+      name: 'Priorité',
+      selector: row => row.Pr,
+    },
+    {
+      name: 'Dernière validation',
+      selector: row => row.Dv,
+    },
+    {
+      name: 'Action',
+      button: true,
+      cell: row => (
+        <Button
+          style={{ color: 'black' }}
+          onMouseEnter={() => setHoveredRow(row)}
+          onMouseLeave={() => setHoveredRow(null)}
+          onClick={() => navigate(`/dashboard/chaine-de-valeur`, { replace: true })}
+        >
+          {hoveredRow === row ? (
+            <VisibilityIcon style={{ color: '#495F52', fontSize: 30 }} />
+          ) : (
+            <CircleRoundedIcon style={{ color: 'gray', fontSize: 30 }} />
+          )}
+        </Button>
+      ),
+    },
+  ];
+
+  const [hoveredRow, setHoveredRow] = useState(null);
+
+  let data = [
+    // data here...
+  ];
+  data = [..."_".repeat(400000)].map((_, i)=> data[i%data.length]);
+
+  const [filterText, setFilterText] = useState('');
+  const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
+
+  const filteredItems = data.filter((item) => item.Nc && item.Nc.toLowerCase().includes(filterText.toLowerCase()));
+
+  const subHeaderComponentMemo = React.useMemo(() => {
+    const handleClear = () => {
+      if (filterText) {
+        setResetPaginationToggle(!resetPaginationToggle);
+        setFilterText('');
+      }
+    };
+    return (
+      <FilterComponent onFilter={(e) => setFilterText(e.target.value)} onClear={handleClear} filterText={filterText} />
+    );
+  }, [filterText, resetPaginationToggle]);
+
+  return (
+    <>
+      <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+        <Typography variant="h4" gutterBottom>
+          Clients
+        </Typography>
+      </Stack>
+      <Card>
+        <Scrollbar>
+          <DataTable
+            columns={columns}
+            data={filteredItems}
+            pagination
+            paginationResetDefaultPage={resetPaginationToggle}
+            subHeader
+            subHeaderComponent={subHeaderComponentMemo}
+            persistTableHead
+          />
+        </Scrollbar>
+      </Card>
+    </>
+  );
+}
 

fb1235f17bb956a884f949c02331145a69265199
Manfaily Roland Yao <68975545+07Manfaily@users.noreply.github.com>
2024-02-15 16:30:12 +0000

    Update LoginForm.js

--- src/sections/auth/login/LoginForm.js
+++ src/sections/auth/login/LoginForm.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
 // @mui
 import { Stack, IconButton, InputAdornment, TextField, Checkbox, Typography } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
@@ -14,25 +16,60 @@ export default function LoginForm() {
   const navigate = useNavigate();
 
   const [showPassword, setShowPassword] = useState(false);
-  const [error, setError] = useState('')
-  const [username, setUsername]  = useState('')
-  const [password, setPassword]  =  useState('')
+  const [error, setError] = useState('');
+  const [matricule, setMatricule] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    if (username === '' || password === '') {
+  const handleLogin = async () => {
+    if (matricule === '' || password === '') {
       setError('Les champs sont vides');
-    } else { 
-      setError(''); // Effacez les erreurs précédentes s'il y en avait
-      navigate(`/dashboard`, { replace: true }); 
+    } else {
+      await axios
+      .post('/user/login', { matricule: matricule, password: password })
+      .then((response) => {
+        console.log("reponse de l'api du login: ", response);
+        const userData = response.data.data;
+        localStorage.setItem('accessToken', userData[0].token.access_token);
+        localStorage.setItem('username', userData[0].user);
+
+          // const matricule = data.first_name+" "+data.last_name
+
+          if (response.status === 200) {
+            navigate(`/dashboard/chaine-de-valeur`, { replace: true });
+          } else {
+            console.log('Mauvaise information de connexion');
+            setError('Mauvaise information de connexion');
+          }
+          if (response.status === 500) {
+            setError('erreur liée au serveur');
+          } else {
+            setError('');
+          }
+        })
+        .catch((error) => {
+          console.log('Error lors du traitement:', error);
+          setError('Oups !!!! erreur liée au serveur');
+          console.log(error.response?.data || "Oups!!!! une erreur s'est produite");
+        });
     }
   };
+
   return (
     <>
       <Stack spacing={3}>
-      { error ?  <Alert variant="outlined" severity="error">
-           {  error}
-      </Alert>: ""}
-        <TextField name="email" label="Email address"  value={username} onChange={(e) => setUsername(e.target.value)} />
+        {error ? (
+          <Alert variant="outlined" severity="error">
+            {error}
+          </Alert>
+        ) : (
+          ''
+        )}
+        <TextField
+          name="identifiant"
+          label="Identifiant"
+          value={matricule}
+          onChange={(e) => setMatricule(e.target.value)}
+        />
         <TextField
           name="password"
           label="Password"
@@ -56,11 +93,7 @@ export default function LoginForm() {
           <Checkbox name="remember" />
           <Typography variant="body2">Se souvenir de moi</Typography>
         </div>
-
-       
-
       </Stack>
-    
 
       <LoadingButton fullWidth size="large" type="submit" variant="contained" onClick={handleLogin}>
         Se connecter
